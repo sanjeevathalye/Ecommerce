31c74b777ca51f06751895368b65c0024f604f10
sanjeevathalye <sanjeevathalye@yahoo.com>
2020-06-07 22:20:01 -0400

    implemented horizontal navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  isOpen = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleNavBar() {
+    this.isOpen = !this.isOpen ;
   }
 
 }
